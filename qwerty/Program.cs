7defaf13ab758b5860fed98944170ed191cc0237
viktor <moiseev0506@mail.ru>
2022-11-10 22:17:28 +0300

    version 1

---
+++ qwerty/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qwerty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                array.Add(random.Next(1, 50));
+            }
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(Qwert(array));    
+
+
+            Console.ReadKey();
+
+        }
+        public static int Qwert(List<int>array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i]<25)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
